f5934108d756f937768faf5ea45a113ba2dbbbf9
uyo2004 <uokene@ivytech.edu>
2026-08-25 10:58:02 -0400

    first program

---
+++ ch2/DataDemo.java
@@ -0,0 +1,9 @@
+public class DataDemo
+{
+    public static void main(String[] args)
+    {
+        int aWholeNumber = 315;
+        System.out.print("The number is ");
+        System.out.println(aWholeNumber);
+    }
+}
