7ca8055d5af528dc54fdb27d6d165b01fada2358
uyo2004 <uokene@ivytech.edu>
2026-08-31 15:46:48 -0400

    ch1-ex7

---
+++ ch1-ex7/MovieQuoteInfo.java
@@ -0,0 +1,13 @@
+// Uyoojo Okene
+// p.35 = Exercise 7
+
+public class MovieQuoteInfo {
+
+    public static void main(String[] args) {
+        System.out.println("Famous quote: \"Happiness can be found, even in the darkest of times, if one only remembers to turn on the light\"");
+        System.out.println("Name of the movie: Harry Potter and the Prisoner of Azkaban");
+        System.out.println("Name of the character: Albus Dumbledore");
+        System.out.println("Year of release: 2004");
+
+    }
+}
