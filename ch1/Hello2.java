a67b0431360026c33a268ce6b7d3f43a275d770f
uyo2004 <uokene@ivytech.edu>
2026-08-31 14:32:13 -0400

    chapter 1

---
+++ ch1/Hello2.java
@@ -0,0 +1,10 @@
+// Uyoojo Okene
+// p.17
+// Filename Hello2.java
+// Written on August 31, 2026
+public class Hello2 {
+/* This is a simple Java program that prints "Hello, world!" to the console. */
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
---
+++ ch1/HelloDialog.java
@@ -0,0 +1,8 @@
+// Uyoojo Okene
+// p.29 Aug, 31
+import javax.swing.JOptionPane;
+public class HelloDialog {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello, world!");
+    }
+}
