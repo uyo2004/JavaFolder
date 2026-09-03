ad37e8b9a155fdef27d64dc273aeb1687ef266e1
uyo2004 <uokene@ivytech.edu>
2026-09-03 14:31:31 -0400

    ch3 youdoit

---
+++ ch3/ch3 youdoit/ParadiseInfo.java
@@ -0,0 +1,11 @@
+//Uyoojo Okene
+//p.89
+public class ParadiseInfo {
+       public static void main(String[] args) {
+              displayInfo();
+       }
+       public static void displayInfo() {
+              System.out.println("Pradise Day Spa wants to pamper you.");
+              System.out.println("We will make you feel good.");
+       }
+}
---
+++ ch3/ch3 youdoit/TestInfo.java
@@ -0,0 +1,10 @@
+//Uyoojo Okene
+//p.90
+public class TestInfo {
+
+
+	public static void main(String[] args) {
+		System.out.println("Calling method from another class:");
+        ParadiseInfo.displayInfo();
+	}
+}
