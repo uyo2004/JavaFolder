dd1e273e453e3243b337ad3c8b5bb02d044a1ba1
uyo2004 <uokene@ivytech.edu>
2026-09-16 15:57:04 -0400

    all

--- ch6/ch6youdoit/DivideEvenly.java
+++ ch6/ch6youdoit/DivideEvenly.java
@@ -1,2 +1,16 @@
 // Uyoojo Okene
 // p. 154
+
+public class DivideEvenly {
+
+    public static void main(String[] args) {
+        final int limit = 100;
+        int var;
+        System.out.println(limit + "is evenly divisible by ");
+        for (var = 1; var <= limit; ++var) {
+            if (limit % var == 0)
+                System.out.print(var + " ");
+            System.out.println();
+        }
+    }
+}
---
+++ ch6/ch6youdoit/EvenEntryLoop.java
@@ -0,0 +1,20 @@
+// Uyoojo Okene
+// p. 232
+
+public class EvenEntryLoop {
+
+    public static void main(String[] args) {
+        int number = 5;
+        while (number != 999) {
+            if (number % 2 == 0) {
+                System.out.print("Good job! ");
+                number = 999;
+            } else {
+                System.out.print("Not an even number ");
+                number = 999;
+            }
+        System.out.println("Thank you for running the program.");
+        }
+    }
+}
+
